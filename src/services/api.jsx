af340354389dc83007a8ba69eb65922bcea1643f
Toni Andström <toni_andstrom@hotmail.com>
2024-10-21 22:30:08 +0300

    removed a screenshot that was not used

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:49965/jsonapi';
+const API_URL = 'http://localhost:50052/jsonapi';
 
 export const fetchContent = async (contentType) => {
   const response = await axios.get(`${API_URL}/${contentType}`);
